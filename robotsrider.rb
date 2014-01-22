06ef8713526f771382e0f10bb74f8cd2e8e9ecca
Felipe Molina <felmoltor@gmail.com>
2014-01-22 18:45:02 +0100

    Add option '-d' with theharvester and fix some bugs

--- robotsrider.rb
+++ robotsrider.rb
@@ -6,22 +6,24 @@ require 'optparse'
 require 'logger'
 require 'uri'
 require 'colorize'
+require 'nokogiri'
 
 class RobotsRider
   def initialize(options)
     @urlfile = options[:urlfile]
+    @domain = options[:domain]
+    @domain.gsub!("http://","")
+    @domain.gsub!("https://","")
     @takesnapshot = options[:takesnapshot]
     @visit = options[:visit]
+    @log = Logger.new("logs/#{Time.now.strftime('%Y%m%d_%H%M%S')}_robotsrider.log")
+    
     if (!options[:urlfile].nil? and File.exists?(options[:urlfile]))
       @randomsearch = false 
     else
       @randomsearch = true
     end
-    if !options[:logfile].nil?
-      @log = Logger.new(options[:logfile])
-    else
-      @log = Logger.new("logs/#{Time.now.strftime('%Y%m%d_%H%M%S')}_robotsrider.log")
-    end
+    
     if !options[:loglevel].nil?
       if options[:loglevel] = "DEBUG"
         @log.level = Logger::DEBUG
@@ -52,6 +54,16 @@ class RobotsRider
         @juicywords << jline.upcase.gsub(/\s+/," ").strip
       }
     end
+    
+    # Initialize the URL file if there was a domain specified and the user has 
+    # theHarvester in hist PATH.
+    if !@domain.nil?
+      
+      newurlfile = queryTheHarvester
+      if !newurlfile.nil?
+        @urlfile = newurlfile
+      end
+    end
     # @stats = {}
   end
   
@@ -63,6 +75,23 @@ class RobotsRider
         return true 
       end
     }
+    return false
+  end
+  
+  #############
+  
+  def hasJuicyTitle(htmlcode)
+    html_doc = Nokogiri::HTML(htmlcode)
+    pagetitle = html_doc.css('title').text.upcase.gsub(" ","")
+    
+    jtf = File.open("juicytitles.list","r")
+    jtf.each {|jtitle|
+      if pagetitle == jtitle.upcase.gsub(" ","")
+        jtf.close
+        return true
+      end
+    }
+    jtf.close
     return false
   end
   
@@ -97,10 +126,73 @@ class RobotsRider
     return false
   end
   
+  
+  ##########################
+  
+  def getTheHarvesterPath()
+    # Check if theharvester is in the path
+    whereisoutput = `whereis theharvester`
+    thpaths = whereisoutput.split(":")[1]
+    thpaths.split(" ").each {|path|
+      if `file -i #{path}`.split(":")[1].strip.split(";")[0].strip == "text/x-python"
+        return path
+      end      
+    }
+    return nil
+  end
+  
+  ##########################
+  
+  def queryTheHarvester()
+    # This function calls the programwhereis theharvester theHarvester automaticaly to harvest URLs from a domain
+    # Instead of providing the program with a list of urls we can directly ask for a domain
+    resultfile = "harvester_output_#{@domain}.list"
+    thtmpfile = "#{@domain}.html"
+    thbin = getTheHarvesterPath
+    
+    if !thbin.nil?
+      # Create an URL file with the outuput of the harvester for hosts
+      # Retrieve the host found in the domain provided by the user
+      cmdline = "#{thbin} -f #{thtmpfile} -d #{@domain} -b all "
+      puts "Searching with 'theharvester' information about the domain '#{@domain}'. Please, be patient."
+      @log.debug "Searching with 'theharvester' information about the domain #{@domain}"
+      @log.debug " #{cmdline}"
+      salida = `#{cmdline}`
+      if File.exists?(thtmpfile)
+        fdomain = File.open(thtmpfile)
+        html_doc = Nokogiri::HTML(fdomain)
+        fdomain.close
+        # Busca los <li> <ul class="softlist"> y <ul class="pathslist">   
+        hostfound = []
+        html_doc.xpath("//li[@class='softitem']").each { |sitem|
+          # puts "softitem: #{sitem.content}"
+          hostfound << sitem.content.split(":")[1]
+        }
+        html_doc.xpath("//li[@class='pathitem']").each { |pitem|
+          # puts "pathitem #{pitem.content}"    
+          hostfound << pitem.content.split(":")[1]       
+        }
+        hostfound = hostfound.sort.uniq
+        urlsfile = File.open(resultfile,"w")
+        hostfound.each {|h| urlsfile.puts("http://#{h}")}  
+        urlsfile.close        
+      else
+        @log.error "There was some error for 'theHarvester' creating the file '#{@domain}.html'. "
+        puts "There was some error for 'theHarvester' creating the file '#{@domain}.html'. "
+        resultfile = nil
+      end
+    else
+      @log.critical "You don't have the tool 'theharvester' installed. Donwload and install it before using option '-d' again (https://code.google.com/p/theharvester/downloads/list)"
+      puts "You don't have the tool 'theharvester' installed. Donwload and install it before using option '-d' again (https://code.google.com/p/theharvester/downloads/list)"
+      resultfile = nil
+    end
+    File.delete(thtmpfile)
+    return resultfile    
+  end
+  
   #############
   
   def rideRobots()
-    
     # Create folder for visited in this execution
     visiteddir = "visited/#{Time.now.strftime('%Y%m%d_%H%M%S')}/"
     if @visit
@@ -117,6 +209,7 @@ class RobotsRider
       begin
         uri = URI.parse(url)
         robotsurl = "#{uri.scheme}://#{uri.host}/robots.txt"
+        # TODO: Change timeout for the HTTP connexion (https://stackoverflow.com/questions/13074779/ruby-nethttp-idle-timeout)
         robots_response = Net::HTTP.get_response(URI(robotsurl))
         if robots_response.code.to_i == 200
           robots_body = robots_response.body
@@ -134,8 +227,7 @@ class RobotsRider
                     
                     disurl = "#{uri.scheme}://#{uri.host}/#{prohibido}"
                     @log.debug "Found '#{disurl}' as a disallowed entry."
-                    puts "Found '#{disurl}' as a disallowed entry."
-                    
+                    puts "Found '#{disurl}' as a disallowed entry."                    
                       
                     # TODO: Save in summary the results
                     if @visit
@@ -146,16 +238,20 @@ class RobotsRider
                         dis_response = Net::HTTP.get_response(URI(disurl))
                         if dis_response.code.to_i == 200
                           # Search for juicy words in the url
+                          @log.debug "URL '#{disurl}' exists."
+                          puts "URL '#{disurl}' exists."  
+                          
                           if hasJuicyFiles(prohibido)
                             @log.debug "URL '#{disurl}' exists. (And it seems interesting)"
-                            puts "URL '#{disurl}' exists. (And it seems interesting)".red
-                            # Search for juicy words in the body
-                          elsif hasJuicyWords(dis_response.body)
+                            puts " It seems interesting in the Path!".red
+                          end
+                          if hasJuicyWords(dis_response.body)
                             @log.debug "URL '#{disurl}' exists. (And it seems interesting in his body)"
-                            puts "URL '#{disurl}' exists. (And it seems interesting in his body)".red
-                          else
-                            @log.debug "URL '#{disurl}' exists."
-                            puts "URL '#{disurl}' exists.".yellow                          
+                            puts " It seems interesting in his body content!".red
+                          end
+                          if hasJuicyTitle(dis_response.body)
+                            @log.debug "URL '#{disurl}' exists. (And it seems interesting in his Title)"
+                            puts " It seems interesting in his page Title!".red
                           end
                           sf = File.open(savefile,"w")
                           sf.write(dis_response.body)
@@ -179,8 +275,8 @@ class RobotsRider
             @log.warn("Request to #{robotsurl} was not successfull. Skipping.")
           end
         else # if robots_response.code == 200
-          @log.warn("Response code for #{robotsurl} was not 200 (#{robots_response.code}).")
-          puts "Response code for #{robotsurl} was not 200 (#{robots_response.code}).".yellow
+          @log.warn("It seems #{robotsurl} does not exists (#{robots_response.code}).")
+          puts "It seemps #{robotsurl} does not exists (#{robots_response.code}).".yellow
         end        
       rescue URI::BadURIError => e
         @log.error("The specified URL #{url} is not valid. Ignoring...")
@@ -188,34 +284,39 @@ class RobotsRider
       rescue URI::InvalidURIError => e
         @log.error("The specified URL #{url} is not valid. Ignoring...")
         @log.error("Error: #{e.message}") 
-      rescue Errno::ETIMEDOUT => e
-        @log.error("Connexion with #{url} timed out. Probably the port is not open...")
+      rescue Errno::ETIMEDOUT, Timeout::Error => e
+        @log.error("Connexion with #{robotsurl} timed out. Probably the port is not open...")
         @log.error("Error: #{e.message}")     
-        puts "Connexion with #{url} timed out. Probably the port is not open".red    
+        puts "Connexion with #{robotsurl} timed out. Probably the port is not open".red    
       end  
     }
     urlf.close
   end
   
-end
+end # class RobotsRider
 
+##########################
+##########################
 ##########################
 
 def parseOptions()
-  
+  # TODO: Output results to a HTML or XML file to later exploration 
   options = {
     :urlfile => nil,
+    :domain => nil,
     :takesnapshot => false, 
     :visit => true, 
     :crawl => false, 
-    :logfile => nil, 
     :loglevel => nil
   }
   optparse = OptionParser.new do |opts|   
     
     opts.banner = "Usage: #{__FILE__} [OPTIONS]"
      
-    opts.on( '-u', '--urls FILE', String, "(MANDATORY) File containing the list of URLs to check for robots.txt" ) do |file|
+    opts.on( '-d', '--domain DOMAIN', String, "Domain to explore for robots.txt (This option needs program 'theharvester' in your PATH)" ) do |domain|
+      options[:domain] = domain
+    end
+    opts.on( '-u', '--urls FILE', String, "File containing the list of URLs to check for robots.txt" ) do |file|
       options[:urlfile] = file
     end
     opts.on( '-s', '--snapshot','Take a snapshot of the disallowed entries found in robots.txt' ) do
@@ -224,9 +325,6 @@ def parseOptions()
     opts.on( '-v', '--[no-]visit', 'Visit the disallowed entries and record the server response (code and html)' ) do |visit|
       options[:visit] = visit
     end
-    opts.on( '-l', '--logfile [LOGFILE]', 'Set the name of the output log file' ) do |logfile|
-      options[:logfile] = logfile
-    end   
     opts.on( '-L', '--loglevel [LOGLEVEL]', ["DEBUG","WARN","ERROR","CRITICAL"], 'Set loggin level (DEBUG, WARN, ERROR, CRITICAL)' ) do |loglevel|
       if ["DEBUG","WARN","ERROR","CRITICAL"].include?(loglevel)
         options[:loglevel] = loglevel
@@ -240,7 +338,7 @@ def parseOptions()
   
   optparse.parse!
   
-  if options[:urlfile].nil?
+  if options[:urlfile].nil? and options[:domain].nil?
     puts optparse
     raise OptionParser::MissingArgument
   end
@@ -251,14 +349,28 @@ end
 ##########################
 
 def printBanner()
-  puts "*******************************".blue
-  puts "*      ROBOTS RIDER v0.1      *".blue
-  puts "*    Author: Felipe Molina    *".blue
-  puts "*     Twitter: @felmoltor     *".blue
-  puts "*                             *".blue
-  puts "* robots.txt explorer program *".blue
-  puts "*******************************".blue
-  puts
+  unicorn = %q{
+                ,%%%,
+               ,%%%` %==--
+              ,%%`( '|         ,    ,
+             ,%%@ /\_/        (\___/)
+   ,%.-"""--%%% "@@__         (_oo_)
+  %%/             |__`\         (O)
+ .%'\     |   \   /  //       __||__    \)
+ ,%' >   .'----\ |  [/     []/______\[] /
+    < <<`       ||         / \______/ \/
+     `\\\        ||        /    /__\
+       )\\       )\\       (\   /____\
+^^^^^^^"""^^^^^^""^^^^^^^^^^^^^^^^^^^^^^^^^^^
+}
+  puts " ***************************************".cyan
+  puts " *          ROBOTS RIDER v0.2          *".cyan
+  puts " *        Author: Felipe Molina        *".cyan
+  puts " *         Twitter: @felmoltor         *".cyan
+  puts " *                                     *".cyan
+  puts " *    'robots.txt' explorer program    *".cyan
+  puts " ***************************************".cyan
+  puts unicorn.cyan
 end
 
 ##########################
@@ -277,7 +389,6 @@ end
 printBanner()
 op = parseOptions()
 robotsrider = RobotsRider.new(op)
-puts "Riding wild! Wait a little robots are not horses..."
-puts
+# If the user specified a domain, the URLs to explore will be found in the output of the harvester
 summary = robotsrider.rideRobots
 # TODO: Print summary in screen
