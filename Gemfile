af030618f337f3f2dc255a4151aa4cfbf58ceec0
Felipe Molina <felmoltor@gmail.com>
2014-11-25 16:06:38 +0100

    Add bundler gem

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "colorize"
+gem "nokogiri", "~> 1.5"
+
--- robotsrider.rb
+++ robotsrider.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+require 'bundler/setup'
+
 require './classes/RobotsRider'
 require 'optparse'
 require 'colorize'
